3cff4520b7703857918edfdce96d3f2cb5367999
Bas Pennings <baspster@gmail.com>
2017-05-30 17:37:03 +0200

    Tweaks

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # xup2
 Migrations for .NET Core 2.0
+
+# config
+* sqlMigrationPrefix (default `V`)
+* repeatableSqlMigrationPrefix (default `R`)
+* sqlMigrationSeparator (default `__`)
+* sqlMigrationSuffix (default `.sql`)
--- Xup2.Cli/Program.cs
+++ Xup2.Cli/Program.cs
@@ -1,19 +1,47 @@
-﻿using System;
-
-namespace Xup2.Cli
+﻿namespace Xup2.Cli
 {
+    using System;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+
     class Program
     {
-        static void Main(string[] args)
+        private readonly ILogger<Program> logger;
+
+        public Program(ILogger<Program> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Run()
         {
             var path = @"D:\temp\xup";
             var scanner = new FileSystemScanner();
             var resources = scanner.ScanForResources(path, "V", ".sql");
-
             foreach(var r in resources)
             {
                 Console.WriteLine(r.Path);
             }
+        }
+
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            
+            var provider = services.BuildServiceProvider();
+            var program = provider.GetService<Program>();
+            program.Run();            
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            var loggerFactory = new LoggerFactory()
+                .AddConsole();
+            
+            services.AddLogging();
+            services.AddSingleton(loggerFactory);
+            services.AddTransient<Program>();
         }
     }
 }
