73c8002238b283f84106d3dd2c25138be442e9cb
Bas Pennings <baspster@gmail.com>
2017-05-31 16:59:48 +0200

    More formatting

--- Xup2/XupException.cs
+++ Xup2/XupException.cs
@@ -1,17 +1,27 @@
 namespace Xup2
 {
+    using System;
     using System.Runtime.Serialization;
 
     public class XupException : System.Exception
     {
-        public XupException() { }
-        
-        public XupException( string message ) : base( message ) { }
-        
-        public XupException( string message, System.Exception inner ) 
-            : base( message, inner ) { }
-        
-        protected XupException(SerializationInfo info, StreamingContext context ) 
-            : base( info, context ) { }
+        public XupException()
+        {
+        }
+
+        public XupException(string message)
+            : base(message)
+        {
+        }
+
+        public XupException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected XupException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
     }
 }
